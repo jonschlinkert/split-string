37be50d01a65fa42f4512b21fe92ddf4dfb55f7c
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-21 23:22:50 -0500

    refactor

--- index.js
+++ index.js
@@ -1,31 +1,86 @@
 /*!
  * split-string <https://github.com/jonschlinkert/split-string>
  *
- * Copyright (c) 2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
 
-var nonchar = require('noncharacters')[0];
+var extend = require('extend-shallow');
 
-function split(str, ch) {
+module.exports = function(str, options) {
   if (typeof str !== 'string') {
-    throw new TypeError('expected a string.');
+    throw new TypeError('expected a string');
   }
-  ch = ch || '.';
-  var esc = str.split('\\' + ch).join(nonchar);
-  var segs = esc.split(ch);
-  var len = segs.length, i = -1;
-  var res = [];
-  while (++i < len) {
-    res.push(segs[i].split(nonchar).join(ch));
+
+  // allow separator to be defined as a string
+  if (typeof options === 'string') {
+    options = {sep: options};
   }
-  return res;
+
+  var opts = extend({sep: '.'}, options);
+  var arr = [''];
+  var len = str.length;
+  var idx = -1;
+  var closeIdx;
+
+  while (++idx < len) {
+    var substr = str[idx];
+    var next = str[idx + 1];
+
+    if (substr === '\\') {
+      var val = opts.keepEscaping === true ? (substr + next) : next;
+      arr[arr.length - 1] += val;
+      idx++;
+      continue;
+
+    } else {
+      if (substr === '"') {
+        closeIdx = getClose(str, '"', idx + 1);
+        if (closeIdx === -1) {
+          throw new Error('unclosed double quote: ' + str);
+        }
+
+        if (opts.keepDoubleQuotes === true) {
+          substr = str.slice(idx, closeIdx + 1);
+        } else {
+          substr = str.slice(idx + 1, closeIdx);
+        }
+
+        idx = closeIdx;
+      }
+
+      if (substr === '\'') {
+        closeIdx = getClose(str, '\'', idx + 1);
+        if (closeIdx === -1) {
+          throw new Error('unclosed double quote: ' + str);
+        }
+
+        if (opts.keepSingleQuotes === true) {
+          substr = str.slice(idx, closeIdx + 1);
+        } else {
+          substr = str.slice(idx + 1, closeIdx);
+        }
+
+        idx = closeIdx;
+      }
+
+      if (substr === opts.sep) {
+        arr.push('');
+      } else {
+        arr[arr.length - 1] += substr;
+      }
+    }
+  }
+
+  return arr;
+};
+
+function getClose(str, substr, i) {
+  var idx = str.indexOf(substr, i);
+  if (str.charAt(idx - 1) === '\\') {
+    return getClose(str, substr, idx + 1);
+  }
+  return idx;
 }
-
-/**
- * expose `split`
- */
-
-module.exports = split;
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var assert = require('assert');
 var split = require('./');
 
@@ -15,6 +15,33 @@ describe('split', function () {
 
   it('should split a string on dots by default:', function () {
     assert.deepEqual(split('a.b.c'), ['a', 'b', 'c']);
+  });
+
+  it('should respect double-quoted strings', function () {
+    assert.deepEqual(split('"b.c"'), ['b.c']);
+    assert.deepEqual(split('a."b.c"'), ['a', 'b.c']);
+    assert.deepEqual(split('a".b.c"'), ['a.b.c']);
+    assert.deepEqual(split('a."b.c".d'), ['a', 'b.c', 'd']);
+    assert.deepEqual(split('a."b.c".d.".e.f.g.".h'), ['a', 'b.c', 'd', '.e.f.g.', 'h']);
+  });
+
+  it('should respect singlequoted strings', function () {
+    assert.deepEqual(split('\'b.c\''), ['b.c']);
+    assert.deepEqual(split('a.\'b.c\''), ['a', 'b.c']);
+    assert.deepEqual(split('a.\'b.c\'.d'), ['a', 'b.c', 'd']);
+    assert.deepEqual(split('a.\'b.c\'.d.\'.e.f.g.\'.h'), ['a', 'b.c', 'd', '.e.f.g.', 'h']);
+  });
+
+  it('should keep double quotes when options.keepDoubleQuotes is true', function () {
+    assert.deepEqual(split('a."b.c".d', {keepDoubleQuotes: true}), ['a', '"b.c"', 'd']);
+  });
+
+  it('should keep single quotes when options.keepSingleQuotes is true', function () {
+    assert.deepEqual(split('a.\'b.c\'.d', {keepSingleQuotes: true}), ['a', '\'b.c\'', 'd']);
+  });
+
+  it('should keep escaping when options.keepEscaping is true', function () {
+    assert.deepEqual(split('a.b\\.c', {keepEscaping: true}), ['a', 'b\\.c']);
   });
 
   it('should not split on escaped dots:', function () {
