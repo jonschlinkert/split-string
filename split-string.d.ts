785f70f3f69d170ca9d3a5ee954f5b860ffa508b
Ovyerus <iamovyerus@gmail.com>
2018-09-06 17:05:11 +1000

    style: add semicolons

--- split-string.d.ts
+++ split-string.d.ts
@@ -1,32 +1,32 @@
 interface ASTNode {
-  type: 'root' | 'bracket'
-  nodes: ASTNode[]
-  stash: string[]
+  type: 'root' | 'bracket';
+  nodes: ASTNode[];
+  stash: string[];
 }
 
 interface State {
-  input: string
-  separator: string
-  stack: ASTNode[]
-  bos(): boolean
-  eos(): boolean
-  prev(): string
-  next(): string
+  input: string;
+  separator: string;
+  stack: ASTNode[];
+  bos(): boolean;
+  eos(): boolean;
+  prev(): string;
+  next(): string;
 }
 
 interface Options {
-  brackets?: { [key: string]: string } | boolean
-  quotes?: string[] | boolean
-  separator?: string
-  strict?: boolean
-  keep?(value: string, state: State): boolean
+  brackets?: { [key: string]: string } | boolean;
+  quotes?: string[] | boolean;
+  separator?: string;
+  strict?: boolean;
+  keep?(value: string, state: State): boolean;
 }
 
-type SplitFunc = (state: State) => boolean
+type SplitFunc = (state: State) => boolean;
 
-declare function split(input: string): string[]
-declare function split(input: string, options: Options): string[]
-declare function split(input: string, fn: SplitFunc): string[]
-declare function split(input: string, options: Options, fn: SplitFunc): string[]
+declare function split(input: string): string[];
+declare function split(input: string, options: Options): string[];
+declare function split(input: string, fn: SplitFunc): string[];
+declare function split(input: string, options: Options, fn: SplitFunc): string[];
 
-export default split
+export default split;
