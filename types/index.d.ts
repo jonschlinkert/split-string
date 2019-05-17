b035aa036a476bf56ce310757ffc79935bf44ba4
Clement Petit <clement.petit@neo9.fr>
2019-05-17 10:39:24 +0200

    Fix typescript export declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,32 +1,41 @@
 // TypeScript Version: 3.0
 
-export interface ASTNode {
-  type: 'root' | 'bracket';
-  nodes: ASTNode[];
-  stash: string[];
+declare module 'split-string' {
+  function split(input: string, options?: split.Options | split.SplitFunc): string[];
+  function split(input: string, options: split.Options, fn: split.SplitFunc): string[];
+
+  namespace split {
+    interface ASTNode {
+      type: 'root' | 'bracket';
+      nodes: ASTNode[];
+      stash: string[];
+    }
+
+    interface State {
+      input: string;
+      separator: string;
+      stack: ASTNode[];
+
+      bos(): boolean;
+
+      eos(): boolean;
+
+      prev(): string;
+
+      next(): string;
+    }
+
+    interface Options {
+      brackets?: { [key: string]: string } | boolean;
+      quotes?: string[] | boolean;
+      separator?: string;
+      strict?: boolean;
+
+      keep?(value: string, state: State): boolean;
+    }
+
+    type SplitFunc = (state: State) => boolean;
+  }
+
+  export = split;
 }
-
-export interface State {
-  input: string;
-  separator: string;
-  stack: ASTNode[];
-  bos(): boolean;
-  eos(): boolean;
-  prev(): string;
-  next(): string;
-}
-
-export interface Options {
-  brackets?: { [key: string]: string } | boolean;
-  quotes?: string[] | boolean;
-  separator?: string;
-  strict?: boolean;
-  keep?(value: string, state: State): boolean;
-}
-
-type SplitFunc = (state: State) => boolean;
-
-declare function split(input: string, options?: Options | SplitFunc): string[];
-declare function split(input: string, options: Options, fn: SplitFunc): string[];
-
-export default split;
